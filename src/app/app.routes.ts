398179f898d0754596d94a11c2f6778dc0cedf9c
Joao Pedro Fontes <joaopedrofonttes@gmail.com>
2025-05-05 17:05:13 -0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserHomeComponent } from './pages/user-home/user-home.component';
+export const routes: Routes = [
+    {path: 'home', component: UserHomeComponent},
 
-export const routes: Routes = [];
+    {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
---
+++ src/app/pages/user-home/user-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/shared/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-user-home',
+  imports: [SidebarComponent],
+  templateUrl: './user-home.component.html',
+  styleUrl: './user-home.component.css'
+})
+export class UserHomeComponent {
+
+}
