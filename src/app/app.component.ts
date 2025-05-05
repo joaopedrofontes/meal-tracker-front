4f9f82a48d0a1bf3818d44094ca8feb88b9569eb
Joao Pedro Fontes <joaopedrofonttes@gmail.com>
2025-05-05 18:24:25 -0300

    create user-profile page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { UserHomeComponent } from './pages/user-home/user-home.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+
 export const routes: Routes = [
     {path: 'home', component: UserHomeComponent},
+    {path: 'profile', component: UserProfileComponent},
 
     {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
