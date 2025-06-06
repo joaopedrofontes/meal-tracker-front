ee84ba577cea8a2116b4d6a698244a240a8b285e
Joao Pedro Fontes <joaopedrofonttes@gmail.com>
2025-06-06 17:05:16 -0300

    create user menu component

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserMenuComponent } from '../../../comopnents/user/user-menu/user-menu.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [ RouterLink ],
+  imports: [ RouterLink, UserMenuComponent ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
